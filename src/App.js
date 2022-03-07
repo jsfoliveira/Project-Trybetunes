8416e3ce29fae63dafe3e01c1d30cbeb61e5daf5
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-03-07 14:55:54 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
     return (
-      <p>TrybeTunes</p>
+      <BrowserRouter>
+        <p>TrybeTunes</p>
+        <ul>
+          <li><Link to="/">Login</Link></li>
+        </ul>
+        <Switch>
+          <Route path="/" exact component={ Login } />
+          <Route path="/search" exact component={ Search } />
+          <Route path="/album/:id" exact component={ Album } />
+          <Route path="/favorites" exact component={ Favorites } />
+          <Route path="/profile" exact component={ Profile } />
+          <Route path="/profile/edit" exact component={ ProfileEdit } />
+          <Route path="*" exact component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
