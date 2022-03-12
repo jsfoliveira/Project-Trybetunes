098eafb125779e9bf67e73ad9ed16c326351be5f
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-03-12 10:54:35 -0300

    terminando o 8 e 9 ainda

--- src/Components/MusicCard.js
+++ src/Components/MusicCard.js
@@ -1,55 +1,81 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import addSong from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from '../pages/Loading';
+import Input from './input';
 
+// REQUISITO 8: O state inicial é aparecer a mensagem 'carregando...' e o array de objeto vazio (que vai guardar o conjunto de produtos checked).
 class MusicCard extends Component {
   constructor() {
     super();
     this.state = {
       loading: true,
+      checked: [],
     };
   }
 
-  // async saveMusic() {
-  //   await addSong();
-  //   this.setState({
-  //     loading: false,
-  //   });
-  // }
+  // REQUISITO 9
+  async componentDidMount() {
+    await this.takingStorage();
+    this.setState({
+      loading: false,
+    });
+  }
+
+  // REQUISITO 9
+  takingStorage = async () => {
+    const musicFavorites = await getFavoriteSongs();
+    this.setState({
+      checked: musicFavorites,
+    });
+  }
+
+  // REQUISITO 8: Criei uma função para salvar a músicas checkeds. O state é atulizado para true e acontece a seguinte função assíncrona. Se a música estiver marcada, a função async vai receber a promisse addSong, que já está definida no favoriteSongsAPI. Também receberá outra promisse chamada getFavoriteSongs(), essa contém as músicas favoritadas, atribui ela ao checked, isso tudo deve acontecer quando o loading é false, que é quando é possível clicar no checkbox.
+  saveMusic = (marked, song) => {
+    this.setState({
+      loading: true,
+    },
+    async () => {
+      if (marked === true) {
+        await addSong(song);
+        const favorite = await getFavoriteSongs();
+        this.setState({
+          checked: favorite,
+        });
+        this.setState({
+          loading: false,
+        });
+      }
+    });
+  }
 
   render() {
-    const { musics, loading } = this.props;
-    return (
-      loading ? <loading /> : (
+    const { musics } = this.props;
+    const { loading, checked } = this.state;
+    return (loading === true) ? <Loading />
+      : (
         <div>
-          {musics.map((element) => (
-            <div key={ element.trackId }>
-              <h4>{element.trackName}</h4>
-              <audio data-testid="audio-component" src={ element.previewUrl } controls>
+          {musics.map((music) => (
+            <div key={ music.trackId }>
+              <p>{music.trackName}</p>
+              <audio data-testid="audio-component" src={ music.previewUrl } controls>
                 <track kind="captions" />
                 O seu navegador não suporta o elemento
                 {' '}
                 <code>audio</code>
               </audio>
-              <label htmlFor="favorites">
-                <input
-                  id="favorites"
-                  type="checkbox"
-                  value={ element.trackId }
-                  onClick={ this.saveMusic }
-                  data-testid={ `checkbox-music-${element.trackId}` }
-                />
-                Favorita
-              </label>
+
+              <Input
+                music={ music }
+                savingMusics={ this.saveMusic }
+                favs={ checked }
+              />
             </div>))}
         </div>
-      )
-    );
+      );
   }
 }
-
 MusicCard.propTypes = {
-  musics: PropTypes.array.isRequired,
+  musics: PropTypes.array,
 }.isRequired;
-
 export default MusicCard;
---
+++ src/Components/input.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// REQUISITO 8: o favorite inicialmente deve estar false, ou seja, o checkbox não está checked.
+// Eu tive ajuda na mentoria. Percebi que precisaria criar esse outro Componente. Tive muita dificuldade para criar esse requisito 8.
+class Input extends Component {
+  constructor() {
+    super();
+    this.state = {
+      favorite: false,
+    };
+  }
+
+  componentDidMount() {
+    this.favorite();
+  }
+
+  // REQUISITO 8: eu precisei fazer uma função fora para quando o checkbox for checked, o state do favorite fique true, isso quer dizer que.
+  favorite = () => {
+    const { favs, music } = this.props;
+    favs.forEach((e) => {
+      if (e.trackId === music.trackId) {
+        this.setState(
+          { favorite: true },
+        );
+      }
+    });
+  };
+
+  handleChange = ({ target }) => {
+    const { savingMusics, music } = this.props;
+    const { name, checked } = target;
+    this.setState({
+      [name]: checked,
+    }, () => savingMusics(checked, music));
+  }
+
+  render() {
+    const { favorite } = this.state;
+    const { music } = this.props;
+    return (
+      <div>
+        <label htmlFor="favorites">
+          <input
+            id="favorites"
+            name="favorites"
+            type="checkbox"
+            data-testid={ `checkbox-music-${music.trackId}` }
+            onChange={ this.handleChange }
+            checked={ favorite }
+          />
+        </label>
+      </div>
+    );
+  }
+}
+Input.propTypes = {
+  music: PropTypes.obj,
+}.isRequired;
+
+export default Input;
